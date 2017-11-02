37a1f4b72202daca6583f2ce406e0848ce75d314
candy8lee <candy8lee@users.noreply.github.com>
2017-11-02 10:05:20 +0800

    1.0.9
    product_category->DONE
    delete category also delete all

--- final/WAW/admin/product_category/delet.php
+++ final/WAW/admin/product_category/delet.php
@@ -1,6 +1,7 @@
 <?php
 require_once('../../asset/connection/database.php');
 $sth = $db-> query("DELETE FROM product_category WHERE categoryID=".$_GET['cateID']);
-$sth = $db-> query("DELETE FROM q_a_reply WHERE categoryID=".$_GET['cateID']);
+$sth = $db-> query("DELETE FROM product_subcategory WHERE categoryID=".$_GET['cateID']);
+$sth = $db-> query("DELETE FROM product WHERE categoryID=".$_GET['cateID']);
 header('Location: list.php');
  ?>
