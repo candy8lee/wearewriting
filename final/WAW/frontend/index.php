0f9f2a548751ecd4e2cce95c714708706e2ccf61
candy8lee <candy8lee@users.noreply.github.com>
2017-11-06 15:51:13 +0800

    1.4.1
    carousel

--- final/WAW/frontend/index.php
+++ final/WAW/frontend/index.php
@@ -2,7 +2,10 @@
 <head>
 <?php require_once('template/header.php'); ?>
 <link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/index_carousel.css">
+<script src="index_carousel.js">
 
+</script>
 <script>
   $(function(){
     $('#MenuIcon').click(function(){
@@ -14,51 +17,73 @@
 
 <body>
 <?php require_once('template/nav.php'); ?>
-  <div class="py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h1 class="display-4 my-4" contenteditable="true">活動管理</h1>
-          <a class="btn btn-warning my-2" href="add.php"><i class="fa fa-plus-square-o" aria-hidden="true"></i> 新增一筆</a>
+<div class="">
+    <h1 class="text-center">We Are Writing</h1>
+</div>
+<div class="slider-container">
+  <div class="slider-control left inactive"></div>
+  <div class="slider-control right"></div>
+  <ul class="slider-pagi"></ul>
+  <div class="slider">
+    <div class="slide slide-0 active">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Project name 1</h2>
+          <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+          <a href="http://hkmbhutto.wix.com/abdulrasheed"
+          class="slide__text-link">Project link</a>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-12">
-          <ul class="breadcrumb my-3" style="margin-bottom:0px;margin-top:0px">
-            <li class="breadcrumb-item">
-              <a href="list.php">主控台</a>
-            </li>
-            <li class="breadcrumb-item active">活動管理</li>
-          </ul>
-          <table class="table">
-            <thead>
-              <tr>
-                <th>活動開跑至結束日期</th>
-                <th>標題</th>
-                <th>編輯</th>
-                <th>刪除</th>
-              </tr>
-            </thead>
-            <tbody>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tbody>
-          </table>
+    </div>
+    <div class="slide slide-1 ">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Project name 2</h2>
+          <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+          <a href="http://hkmbhutto.wix.com/abdulrasheed"
+          class="slide__text-link">Project link</a>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-12">
-          <ul class="pagination float-right">
-            <?php include_once("template/page_num.php"); ?>
-          </ul>
+    </div>
+    <div class="slide slide-2">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Project name 3</h2>
+          <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+          <a href="http://hkmbhutto.wix.com/abdulrasheed"
+          class="slide__text-link">Project link</a>
         </div>
       </div>
-	   </div>
-   </div>
+    </div>
+    <div class="slide slide-3">
+      <div class="slide__bg"></div>
+      <div class="slide__content">
+        <svg class="slide__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
+          <path class="slide__overlay-path" d="M0,0 150,0 500,405 0,405" />
+        </svg>
+        <div class="slide__text">
+          <h2 class="slide__text-heading">Project name 4</h2>
+          <p class="slide__text-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio veniam minus illo debitis nihil animi facere, doloremque voluptate tempore quia.</p>
+          <a href="http://hkmbhutto.wix.com/abdulrasheed"
+          class="slide__text-link">Project link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <?php include_once("template/footer.php"); ?>
 <?php include_once("template/copyright.php"); ?>
 </body>
-
 </html>
