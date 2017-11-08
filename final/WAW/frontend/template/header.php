5cc6e5ce693db403ebd3dc057b814d55ffb98801
candy8lee <32888897+candy8lee@users.noreply.github.com>
2017-11-08 09:15:52 +0800

    1.5.3

--- final/WAW/frontend/template/header.php
+++ final/WAW/frontend/template/header.php
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
-  <script src="../asset/js/validator.min.js"></script>
-  <link rel="stylesheet" href="../asset/js/jquery-ui/jquery-ui.css">
-  <script src="../asset/js/jquery.js"></script>
-  <script src="../asset/js/jquery-ui/jquery-ui.js"></script>
-  <link rel="stylesheet" href="../admin/css/style.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!--編輯器-->
-  <script type="text/javascript" src="../asset/tinymce/tinymce.min.js"></script>
-  <script type="text/javascript">
-  tinymce.init({
-    selector: 'textarea',
-    height: 500,
-    menubar: false,
-    plugins: [
-      'advlist autolink lists link image charmap print preview anchor textcolor',
-      'searchreplace visualblocks code fullscreen',
-      'insertdatetime media table contextmenu paste code help'
-    ],
-    toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-    content_css: [
-      '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-      '//www.tinymce.com/css/codepen.min.css']
-  });
-  </script>
-=======
 <?php
 require_once('../asset/connection/database.php');
  ?>
@@ -45,4 +13,21 @@ require_once('../asset/connection/database.php');
 <script src="../asset/js/jquery-ui/jquery-ui.js"></script>
 <link rel="stylesheet" href="../admin/css/style.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
->>>>>>> a461b7ddb2d243a5d4aa1845d95345f83ee00cc2
+  <!--編輯器-->
+<script type="text/javascript" src="../asset/tinymce/tinymce.min.js"></script>
+<script type="text/javascript">
+tinymce.init({
+  selector: 'textarea',
+  height: 500,
+  menubar: false,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor textcolor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste code help'
+  ],
+  toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncentealignright alignjustify | bullist numlist outdent indent | removeformat | help',
+  content_css: [
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//www.tinymce.com/css/codepen.min.css']
+});
+</script>
